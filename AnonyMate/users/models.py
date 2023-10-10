e704fd2a342b14d74abcaac4272f990e4cefffeb
Imukua <ianmukuaa@gmail.com>
2023-10-10 23:07:07 +0300

    updated validators, serializers and models

--- AnonyMate/users/models.py
+++ AnonyMate/users/models.py
@@ -1,52 +1,35 @@
-"""Defines all user models."""
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+class AppUserManager(BaseUserManager):
+	def create_user(self, username, password=None):
+		'''if not email:
+			raise ValueError('An email is required.')'''
+		if not password:
+			raise ValueError('A password is required.')
+		'''email = self.normalize_email(email)'''
+		user = self.model(username=username)
+		user.set_password(password)
+		user.save()
+		return user
+	def create_superuser(self, username, password=None):
+		if not username:
+			raise ValueError('A username is required.')
+		if not password:
+			raise ValueError('A password is required.')
+		user = self.create_user(username, password)
+		user.is_superuser = True
+		user.save()
+		return user
 
 
-class User(AbstractUser):
-    """
-    A custom user model for AnonyMate.
-    This model extends Django's built-in `AbstractUser` model to include 
-    additional fields and functionality specific to AnonyMate.
-
-    Attributes:
-        password -> The users password.
-        username -> The users nickname. 
-    """
-    MOODS = [
-        ('Happy', 'Happy'),
-        ('Sad', 'Sad'),
-        ('Suicidal', 'Suicidal'),
-        ('Relaxed', 'Relaxed'),
-    ]
-
-    LIKES = [
-        ('Anime', 'Anime'),
-        ('Books', 'Books'),
-        ('Movies', 'Movies'),
-    ]
-
-    username = models.CharField(max_length=15, unique=True)
-    password = models.CharField(max_length=12)
-    streak =   models.PositiveIntegerField(default=0)
-    mood = models.CharField(max_length=30, choices=MOODS, blank=True)
-    likes = models.CharField(max_length=30, choices=LIKES, blank=True)
-    registration_date = models.DateTimeField(auto_now_add=True)
-
-    def signup(self, username, password):
-        """
-        Creates a new user with the given username and password.
-        
-        Args:
-        - username (str): The desired username for the new user.
-        - password (str): The desired password for the new user.
-        """
-        self.username = username
-        self.set_password(password)
-        self.save()
-
-    def __str__(self):
-        """
-        Returns a string representation of the user object.
-        """
-        return self.username
+class AppUser(AbstractBaseUser, PermissionsMixin):
+	user_id = models.AutoField(primary_key=True)
+	'''email = models.EmailField(max_length=50, unique=True)'''
+	username = models.CharField(max_length=50)
+	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = ['username']
+	objects = AppUserManager()
+	def __str__(self):
+		return self.username
