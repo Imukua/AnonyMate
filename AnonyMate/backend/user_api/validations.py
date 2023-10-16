ed80a7f567ef01650c0c5dbaa6e0df66a88862f3
Imukua <ianmukuaa@gmail.com>
2023-10-16 11:26:39 +0300

    updated username seriliazation

--- AnonyMate/backend/user_api/validations.py
+++ AnonyMate/backend/user_api/validations.py
@@ -3,12 +3,12 @@ from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 def custom_validation(data):
-    email = data['email'].strip()
+    #email = data['email'].strip()
     username = data['username'].strip()
     password = data['password'].strip()
     ##
-    if not email or UserModel.objects.filter(email=email).exists():
-        raise ValidationError('choose another email')
+    '''if not email or UserModel.objects.filter(email=email).exists():
+        raise ValidationError('choose another email')'''
     ##
     if not password or len(password) < 8:
         raise ValidationError('choose another password, min 8 characters')
@@ -18,11 +18,11 @@ def custom_validation(data):
     return data
 
 
-def validate_email(data):
+'''def validate_email(data):
     email = data['email'].strip()
     if not email:
         raise ValidationError('an email is needed')
-    return True
+    return True'''
 
 def validate_username(data):
     username = data['username'].strip()
