6ff1640108976f596dc429a7f8d9fd27d9b4daef
Imukua <ianmukuaa@gmail.com>
2023-10-16 10:14:17 +0300

    removed email field, using username

--- AnonyMate/backend/user_api/models.py
+++ AnonyMate/backend/user_api/models.py
@@ -3,22 +3,21 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 class AppUserManager(BaseUserManager):
-	def create_user(self, email, password=None):
-		if not email:
-			raise ValueError('An email is required.')
+	def create_user(self, username, password=None):
+		if not username:
+			raise ValueError('A username is required.')
 		if not password:
 			raise ValueError('A password is required.')
-		email = self.normalize_email(email)
-		user = self.model(email=email)
+		user = self.model(username=username)
 		user.set_password(password)
 		user.save()
 		return user
-	def create_superuser(self, email, password=None):
-		if not email:
-			raise ValueError('An email is required.')
+	def create_superuser(self, username, password=None):
+		if not username:
+			raise ValueError('A username is required.')
 		if not password:
 			raise ValueError('A password is required.')
-		user = self.create_user(email, password)
+		user = self.create_user(username=username, password=password)
 		user.is_superuser = True
 		user.save()
 		return user
@@ -26,10 +25,9 @@ class AppUserManager(BaseUserManager):
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
 	user_id = models.AutoField(primary_key=True)
-	email = models.EmailField(max_length=50, unique=True)
-	username = models.CharField(max_length=50)
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
+	username = models.CharField(max_length=50, unique=True)
+	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = []
 	objects = AppUserManager()
 	def __str__(self):
 		return self.username
