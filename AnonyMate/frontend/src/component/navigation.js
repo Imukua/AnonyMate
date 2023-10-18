699b7093edf160718d03ad624bdf045bc844c687
Imukua <ianmukuaa@gmail.com>
2023-10-18 14:21:18 +0300

    updated login page reference

--- AnonyMate/frontend/src/component/navigation.js
+++ AnonyMate/frontend/src/component/navigation.js
@@ -17,7 +17,7 @@ export function Navigation() {
                 </Nav>
                 <Nav>
                     {isAuth ? <Nav.Link href="/logout">Logout</Nav.Link> :
-                              <Nav.Link href="/Login">Login</Nav.Link>}
+                              <Nav.Link href="/login">Login</Nav.Link>}
                 </Nav>
             </Navbar>
         </div>
