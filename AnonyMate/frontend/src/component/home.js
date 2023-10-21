df39da2b52a36fdcc3b7992194aa3f419b610eb5
Imukua <ianmukuaa@gmail.com>
2023-10-21 12:31:36 +0300

    added home page

---
+++ AnonyMate/frontend/src/component/home.js
@@ -0,0 +1,33 @@
+// Import the react JS packages
+import { useEffect, useState } from "react";
+import axios from "axios";
+// Define the Login function.
+export const Home = () => {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    if (localStorage.getItem("access_token") === null) {
+      window.location.href = "/login";
+    } else {
+      const accessToken = localStorage.getItem("access_token");
+      console.log(accessToken);
+      (async () => {
+        try {
+          const { data } = await axios.get("http://localhost:8000/home/", {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${accessToken}`,
+            },
+          });
+          setMessage(data.message);
+        } catch (e) {
+          console.log(e);
+        }
+      })();
+    }
+  }, []);
+  return (
+    <div className="form-signin mt-5 text-center">
+      <h3>Hi {message}</h3>
+    </div>
+  );
+};
