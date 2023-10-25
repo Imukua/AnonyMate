889a8510d14fe993eff9a5320be9d9eedf4d5a71
Imukua <ianmukuaa@gmail.com>
2023-10-25 15:59:10 +0300

    added new urls for new methods made

--- AnonyMate/backend/support_groups/urls.py
+++ AnonyMate/backend/support_groups/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
 	path('create/', views.SupportGroupCreate.as_view(), name='create_group'),
-    path('list/', views.SupportGroupListView.as_view(), name='support-group-list'),
-    path('members/', views.GroupMembersListView.as_view(), name='user-support-group-list'),
-    path('list/<int:group_id>/', views.SupportGroupbyIdView.as_view(), name='group-by-id'),
+    path('all/list/', views.SupportGroupListView.as_view(), name='all-support-group-list'),
+    path('all/members/', views.GroupMembersListView.as_view(), name='all-support-groups-and-members'),
+    path('<int:group_id>/', views.SupportGroupbyIdView.as_view(), name='support-group-by-id'),
+    path('<int:group_id>/members/', views.GroupMembersListView.as_view(), name='support-group-members-by-id'),
+    path('<int:group_id>/update/', views.GroupMembersListView.as_view(), name='support-group-members-by-id'),
+    path('', views.GroupMembersListView.as_view(), name='support-group-members-by-id'),
 ]
