a85c4cbb10ea924ed45757768ff5abbf9b019a1a
Imukua <ianmukuaa@gmail.com>
2023-10-25 09:38:26 +0300

    updated view names for support grous

--- AnonyMate/backend/support_groups/urls.py
+++ AnonyMate/backend/support_groups/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	path('create/', views.SupportGroupCreate.as_view(), name='create_group'),
     path('list/', views.SupportGroupListView.as_view(), name='support-group-list'),
-    path('members/', views.UserGroupListView.as_view(), name='user-support-group-list'),
-    path('list/<int:group_id>/', views.SupportGroupListView.as_view(), name='group-by-id'),
+    path('members/', views.GroupMembersListView.as_view(), name='user-support-group-list'),
+    path('list/<int:group_id>/', views.SupportGroupbyIdView.as_view(), name='group-by-id'),
 ]
--- AnonyMate/backend/support_groups/views.py
+++ AnonyMate/backend/support_groups/views.py
@@ -45,7 +45,7 @@ class SupportGroupListView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({"detail": "Support groups not found."}, status=status.HTTP_400_BAD_REQUEST) 
-class UserGroupListView(APIView):
+class GroupMembersListView(APIView):
     serializer_class = UserSupportGroupSerializer
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
@@ -58,7 +58,7 @@ class UserGroupListView(APIView):
         else:
             return Response({"detail": "Support groups not found."}, status=status.HTTP_400_BAD_REQUEST) 
 
-class SupportGroupDetailsView(APIView):
+class SupportGroupbyIdView(APIView):
     serializer_class = SupportGroupSerializer
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
