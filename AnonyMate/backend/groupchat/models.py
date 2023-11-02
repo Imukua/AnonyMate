2ea7ff50179ea37f1d8ca1e9ca7171c1af69f00f
Imukua <ianmukuaa@gmail.com>
2023-11-02 22:01:41 +0300

    added chatmessage model fro persistance

--- AnonyMate/backend/groupchat/models.py
+++ AnonyMate/backend/groupchat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from user_api.models import AppUser
+from support_groups.models import SupportGroups
 # Create your models here.
+class ChatMessage(models.Model):
+    sender = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    group = models.ForeignKey(SupportGroups, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
